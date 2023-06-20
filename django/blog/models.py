aba9daf070cfc4ee51d81e2bddbf375d456bd2a2
gns0314 <gns0314@naver.com>
2023-06-20 16:02:26 +0900

    Django

---
+++ django/blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    writer = models.CharField(max_length=10)
+    # Post테이블이 새로 생성 될떄 시간
+    create_at = models.DateTimeField(auto_now_add=True) 
+    # 수정이 될 때, 그 시간 저장
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    writer = models.CharField(max_length=10)
+    create_at = models.DateTimeField(auto_now_add=True)
