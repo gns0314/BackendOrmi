11396b748c89156640e491ebe14648f68a6b59a1
gns0314 <gns0314@naver.com>
2023-07-03 16:01:16 +0900

    Django

--- django/blog/models.py
+++ django/blog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 # Create your models here.
-
-
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-    writer = models.CharField(max_length=10)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
     # Post테이블이 새로 생성 될떄 시간
     created_at = models.DateTimeField(auto_now_add=True) 
     # 수정이 될 때, 그 시간 저장
